8f4fe00be827acc3cc82f6eaf9aa0293b89edd07
amyavi <144570677+amyavi@users.noreply.github.com>
2024-10-14 20:35:01 -0300

    feat: remove create deco "placard tooltip was null" log

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     ))!!)
 
     modCompileOnly(libs.cctweaked)
+    modCompileOnly(libs.createdeco)
     modCompileOnly(libs.forgeconfigapiport)
     modCompileOnly(libs.fwaystones)
     modCompileOnly(libs.modonomicon)
